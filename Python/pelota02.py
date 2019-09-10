4f03ff795557ecaa5ea10fbe0f6de02a76d5854f
Carolina Jijon Romero <caroolinajr@ciencias.unam.mx>
2019-09-10 15:00:12 -0500

    Se agregaron archivos a la carpeta python

---
+++ Python/pelota02.py
@@ -0,0 +1,13 @@
+# 
+# Carolina Jijón Romero
+# python 3.7.3
+# 10/09/2019
+#
+#
+# Calcula la posición de una pelota con una velocidad inicial
+# de 5 m/s y un tiempo de 0.6 segundos
+v0 = 5
+g = 9.81
+t = 0.6
+y = v0*t - .5*g*t**2
+print(y)
