653b31cd11d73c1a08d7c9f11d7a9bff0c25204e
David Alonso Garduño Granados <alonso2499@ciencias.unam.mx>
2019-09-22 13:35:01 -0500

    Se completó el ejercicio asignado en classroom Ejercicio01, se comentó ejPlot y se agregó el archivo de asistencia de la semana

--- Python/ejPlot.py
+++ Python/ejPlot.py
@@ -20,7 +20,7 @@ y3 = r*sin(3*a)
 y4 = r*sin(4*a)
 y5 = r*sin(5*a)
 
-x = np.array([x1,x2,x3,x4,x5,x1])
+x = np.array([x1,x2,x3,x4,x5,x1]) # Asigna a x el valor de la lisya definida en entre los corchetes
 y = np.array([y1,y2,y3,y4,y5,y1])
 
 
@@ -34,20 +34,20 @@ D = (%5.2f,%5.2f)
 E = (%5.2f,%5.2f)
 """%(r, x1, y2, x2, y2, x3, y3 ,x4 ,y4 ,x5 ,y5))
 
-circulo = plt.Circle((0,0), radius=r, color='b')
-ax=plt.gca()
-ax.spines['left'].set_position('center')
-ax.spines['bottom'].set_position('center')
+circulo = plt.Circle((0,0), radius=r, color='b') #Estamos definiendo las características del círculo
+ax=plt.gca() #definimos los ejes
+ax.spines['left'].set_position('center') #Centramos el eje horizontal
+ax.spines['bottom'].set_position('center') #Centramos el eje vertical
 
-plt.gca().set_aspect('equal', adjustable='box')
-plt.plot(x, y, label='linear', color='r')
-ax.add_patch(circulo)
+plt.gca().set_aspect('equal', adjustable='box') #definimos que la apriencia sea simétrica
+plt.plot(x, y, label='linear', color='r') #Los valores guardados en x y y se piden mostrar y establecemos las características de los mismos
+ax.add_patch(circulo) #añadimos el circulo
 
 
 
-plt.legend()
-plt.show() # comenten esta linea desde el inicio y descomenten la siguientem ¿hubo algun cambio?
-#plt.draw() 
+plt.legend() #muestra la etiqueta como una leyenda
+#plt.show() # 
+plt.draw() #Sobrepone las figuras
 
 l = 3
 a = 2*pi/l
