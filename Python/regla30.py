5339196ea4fb0c00dbdffcfe9114da2f89b8287d
Carolina Jijon Romero <caroolinajr@ciencias.unam.mx>
2019-10-24 13:16:22 -0500

    Se agregaron algunos archivos que se trabajaron en clase sobre el repositorio de mi compañera, ya voy a hacer el add en mi casa, profe :C

---
+++ Python/regla30.py
@@ -0,0 +1,33 @@
+n=43
+l=[0]*n
+l[n//2]=1
+print(l)
+def r30(a,i,s):
+    if [a,i,s]==[1,0,0]:
+        r=1
+    if [a,i,s]==[0,1,0]:
+        r=1
+    if [a,i,s]==[0,0,1]:
+        r=1
+    if [a,i,s]==[0,1,1]:
+        r=1
+    if [a,i,s]==[0,0,0]:
+        r=0
+    if [a,i,s]==[1,1,0]:
+        r=0
+    if [a,i,s]==[1,0,1]:
+        r=0
+    if [a,i,s]==[1,1,1]:
+        r=0
+    return(r)
+ne=[]
+for i in range(n):
+	ne.append(r30(l[i-1],l[i],l[(i+1)%n]))
+l=ne.copy()
+print(l)
+for j in range(22):
+    ne=[]
+    for i in range(n):
+            ne.append(r30(l[i-1],l[i],l[(i+1)%n]))
+    l=ne.copy()
+    print(l)
