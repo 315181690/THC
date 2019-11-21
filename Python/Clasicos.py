2681c5cffb247957617d27afb19a23b5aadd00ac
Carolina Jijon Romero <caroolinajr@ciencias.unam.mx>
2019-11-21 14:58:04 -0600

    Se agegaron archivos a Python y LaTeX

---
+++ Python/Clasicos.py
@@ -0,0 +1,145 @@
+'''
+
+    Algunos ejercicios clasicos
+
+'''
+
+def celciusAfahrenheit(c):
+    resultado=(c*(9/5))+32
+    '''
+Convertir grados celcius a grados Fahrenheit
+'''
+    return(resultado)
+
+
+def fahrenheitAcelcius(f):
+    resultado= (f-32)*(9/5)
+    '''
+Convertir grados Fahrenheit a grados celcius
+'''
+    return(resultado)
+
+def alturaY(y):
+    
+    '''
+Considerando la ecuacion
+y(t)= v0*t - 1/2*g*t**2
+implementar una funcion que dada una altura yc
+muestre el tiempo que tardo la pelota en llegar a yc
+'''
+
+
+# En los siguientes ejercicios se tiene que definir la funcion
+# de principio a fin, es decir; desde definir el nombre y los datos de entrada
+# hasta determinar si la funcion debe regresar algun valor o no(datos de salida)
+
+'''
+Implementar una funcion que convierta segundos a años
+por ejemplo, un billon de segundos a años
+'''
+def segundosAaños(s):
+    resultado = s/3.154e+7
+    return resultado
+
+
+'''
+Implementar una funcion que convierta años a segundos
+por ejemplo, un 16 años a segundos
+'''
+def añosAsegundos(a):
+    resultado = s*3.154e+7
+    return resultado
+
+'''
+Convertir metros a pulgadas
+'''
+def metroApulgada(m):
+    resultado= 39.37*m
+    return resultado
+
+'''
+convertir metros a pies
+'''
+def metroApie(m):
+    resultado = 3.28*m
+    return resultado
+'''
+convertir metros a yardas
+'''
+def metroAyarda(m):
+    resultado = 1.094*m
+    return resultado 
+
+'''
+convertir metros a millas
+'''
+def metroAmilla(m):
+    resultado = m/1609.344
+    return resultado
+
+'''
+Dados los numeros a y b mostrar
+el desarrollo de
+(a + b)^2
+(a - b)^2
+'''
+def binomio_al_cuadrado
+'''
+Dados los coeficientes de una ecuacion cuadratica
+de segundo grado en una variable (a,b,c) mostrar
+las racies r1 y r2 de la ecuacion
+'''
+
+
+
+##########
+# Verificar que los siguientes bloques de codigo
+# funcionen, en caso de no hacerlo
+# corregirlos (depurarlos)
+#
+
+# programa 1
+from math import sin, cos
+x = pi/4
+1_val = math.sin^2(x) + math.cos^2(x)
+print 1_VAL
+
+# programa 2
+C = A + B
+A = 3
+B = 2
+print C
+
+
+##########
+# Buscar errores del en el archivo con nombre
+#   BuscarErrores.py
+# comentar que es lo que esta mal en cada linea
+
+
+'''
+
+    Recuerden:
+
+/  es la division flotante
+// es la division entera
+%  es el modulo
+
+no olviden dejar un espacio cuando utilicen
+los operadores aritmeticos +, - y con el
+de asignacion =
+
+no utilicen espacios cuando utilicen los
+operadores *, / y **
+
+
+un igual es asignacion, dos iguales son comparacion
+.git es el directorio con el que no hay que "meterse"
+
+y finalmente:
+"los peces son amigos, no comida"
+(bueno esta no creo que sea muy util pero me acorde :) )
+
+'''
+
+
